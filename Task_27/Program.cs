c0fa94333a388e5714e433d7cf3b33719e40309b
yulia <yuliagorbunova@yahoo.com>
2023-01-24 19:34:23 +0500

    HW_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int GetUserDate(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userDate = int.Parse(Console.ReadLine()!);
+    return userDate;
+}
+
+int getSumOfRange(int number)
+{
+    int result = 0;
+    while (number > 10)
+    {
+        int temp = number % 10;
+        number = (number - temp)/10;
+        result = result + temp;  
+    }
+    result = result + number;
+    return result;
+}
+void showData(string messageStart, int data)
+{
+    Console.Write(messageStart);
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+int number = GetUserDate("Введите число");
+int result = getSumOfRange(number);
+showData($"Сумма цифр в числе {number} = ", result);
